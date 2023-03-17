c313123dca130fdb0c15bb07f449671f9e4535d3
ChristianAlvian <christianalviannn@gmail.com>
2023-03-17 14:29:05 +0700

    3

--- app/src/main/java/com/chrisrepo/belajarsharedpreferences/KendaliLogin.java
+++ app/src/main/java/com/chrisrepo/belajarsharedpreferences/KendaliLogin.java
@@ -1,4 +1,43 @@
 package com.chrisrepo.belajarsharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class KendaliLogin {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor spEditor;
+    private Context ctx;
+
+    public String KeyUsername = "/yqng+379kSGtLMYXlSoWw==username" ;
+    public String varUsername1 = "christian";
+    public String varPassword1 = "mia17";
+    public String varUsername2 = "mia";
+    public String varPassword2 = "chrisgantengnian";
+
+    public KendaliLogin(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    public void setPref(String key, String value){
+        sp = PreferenceManager.getDefaultSharedPreferences(ctx);
+        spEditor = sp.edit();
+        spEditor.putString(key, value);
+        spEditor.commit();
+    }
+
+    public String getPref(String key){
+        sp =PreferenceManager.getDefaultSharedPreferences(ctx);
+        return sp.getString(key, null);
+
+    }
+
+    public Boolean isLogin(String key){
+        if (getPref(key)!=null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- app/src/main/java/com/chrisrepo/belajarsharedpreferences/MainActivity.java
+++ app/src/main/java/com/chrisrepo/belajarsharedpreferences/MainActivity.java
@@ -2,13 +2,30 @@ package com.chrisrepo.belajarsharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvUsername;
+    private Button btnLogout;
+    KendaliLogin KL = new KendaliLogin(MainActivity.this);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        if(KL.isLogin(KL.KeyUsername)) == true){
+            setContentView(R.layout.activity_main);
+        }
+        else{
+            startActivities(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+
     }
 }
